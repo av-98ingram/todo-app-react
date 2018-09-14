a48399cfc662d6893ea9a746fe2e103e04cc0a7f
Felipe almeida <felipepereira937@gmail.com>
2018-09-13 21:30:21 -0300

    separação das páginas

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import PageHader from '../template/pageHeader'
 
 export default props => (
   <div>
-    <h1>Sobre</h1>
+    <PageHader name='Sobre' small='Nós'></PageHader>
+
+    <h2>Nossa História</h2>
+    <p>Lorem ipsum dolor sit amet...</p>
+    <h2>Missão e Visão</h2>
+    <p>Lorem ipsum dolor sit amet...</p>
+    <h2>Imprensa</h2>
+    <p>Lorem ipsum dolor sit amet...</p>
   </div>
 )
