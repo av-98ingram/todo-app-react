b20c8c10f8810ed429031bd016bc42e8c3abfacd
Felipe almeida <felipepereira937@gmail.com>
2018-11-03 15:22:51 -0300

    Implementando Middleware

--- src/todo/todoReducer.js
+++ src/todo/todoReducer.js
@@ -1,19 +1,4 @@
-const INITIAL_STATE = {
-  description: 'Ler Livro',
-  list: [{
-    _id: 1,
-    description: 'Pagar Fatura cartão',
-    done: true
-  }, {
-    _id: 2,
-    description: 'Reunião com equipe às 10:00',
-    done: false
-  }, {
-    _id: 3,
-    description: 'Consulta médica',
-    done: false
-  }]
-}
+const INITIAL_STATE = { description: 'Ler Livro', list: [] }
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
